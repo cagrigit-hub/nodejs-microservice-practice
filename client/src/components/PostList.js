44cde2582cbd86e64b3a6296add1ff9bd8ebd555
cagrigit-hub <cagrikutayok@hotmail.com>
2023-02-10 01:45:11 +0300

    first commit

---
+++ client/src/components/PostList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import CommentCreate from './CommentCreate'
+import CommentList from './CommentList'
+function PostList() {
+    const [posts, setPosts] = useState({})
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get('http://localhost:4000/posts')
+            setPosts(res.data)
+        }
+        fetchPosts()
+    }, [])
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {Object.values(posts).map(post => (
+                <div className='card' style={{ width: '30%', marginBottom: '20px' }} key={post.id}>
+                    <div className='card-body'>
+                        <h3>{post.title}</h3>
+                        <CommentList postId={post.id} />
+                        <CommentCreate postId={post.id} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default PostList
---
+++ posts/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express()
+app.use(cors());
+app.use(bodyParser.json());
+
+const posts = {};
+
+
+app.get('/posts' , (req,res) => {
+    res.send(posts);
+})
+
+app.post('/posts', async (req,res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    if(!title) return res.status(400).send({error: 'Title is required'});
+    posts[id] = {
+        id, title
+    };
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    }).catch((err) => {
+        console.log(err.message);
+        });;
+    res.status(201).send(posts[id]);
+})
+
+app.post('/events', (req,res) => {
+    console.log('Received Event', req.body.type);
+    res.send({});
+})
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000')
+})
