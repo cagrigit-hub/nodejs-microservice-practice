f2d0ca6ac76213421b299afb1ad9c455fefc603d
cagrigit-hub <cagrikutayok@hotmail.com>
2023-02-13 16:06:46 +0300

    k8s config

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import CommentCreate from './CommentCreate'
-import CommentList from './CommentList'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 function PostList() {
-    const [posts, setPosts] = useState({})
+  const [posts, setPosts] = useState({});
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const res = await axios.get('http://localhost:4002/posts')
-            setPosts(res.data)
-        }
-        fetchPosts()
-    }, [])
-    return (
-        <div className="d-flex flex-row flex-wrap justify-content-between">
-            {Object.values(posts).map(post => (
-                <div className='card' style={{ width: '30%', marginBottom: '20px' }} key={post.id}>
-                    <div className='card-body'>
-                        <h3>{post.title}</h3>
-                        <CommentList comments={post.comments} />
-                        <CommentCreate postId={post.id} />
-                    </div>
-                </div>
-            ))}
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("http://query-srv:4002/posts");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {Object.values(posts).map((post) => (
+        <div
+          className="card"
+          style={{ width: "30%", marginBottom: "20px" }}
+          key={post.id}>
+          <div className="card-body">
+            <h3>{post.title}</h3>
+            <CommentList comments={post.comments} />
+            <CommentCreate postId={post.id} />
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default PostList
+export default PostList;
--- event-bus/index.js
+++ event-bus/index.js
@@ -1,36 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
 const events = [];
 
-app.get('/events', (req, res) => {
-        res.send(events);
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
-app.post('/events', (req, res) => {
-        const event = req.body;
-        events.push(event);
-        axios.post('http://localhost:4000/events', event).catch((err) => {
-            console.log(err.message);
-          });; // Posts event to posts service
-        axios.post('http://localhost:4001/events', event).catch((err) => {
-            console.log(err.message);
-          });; // Posts event to comments service
-        axios.post('http://localhost:4002/events', event).catch((err) => {
-            console.log(err.message);
-          });; // Posts event to query service
-        axios.post('http://localhost:4003/events', event).catch((err) => {
-            console.log(err.message);
-          });; // Posts event to moderation service
-        res.send({ status: 'OK' });
+app.post("/events", (req, res) => {
+  const event = req.body;
+  events.push(event);
+  axios.post("http://posts-srv:4000/events", event).catch((err) => {
+    console.log(err.message);
+  }); // Posts event to posts service
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  }); // Posts event to comments service
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
+    console.log(err.message);
+  }); // Posts event to query service
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+    console.log(err.message);
+  }); // Posts event to moderation service
+  res.send({ status: "OK" });
 });
-
-
 
 app.listen(4005, () => {
-        console.log('Listening on 4005');
+  console.log("Listening on 4005");
 });
